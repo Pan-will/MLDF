a874a75d74e10e82052eb30a306db04c196b4830
pw <15097686925@163.com>
2020-10-30 08:25:08 +0800

    首提

---
+++ learner/Layer.py
@@ -0,0 +1,72 @@
+import numpy as np
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+
+class Layer:
+    def __init__(self, n_estimators, num_forests, num_labels, step=3, layer_index=0, fold=0):
+        """
+        :param n_estimators: 森林中树的数量=40
+        :param num_forests: 森林数量=2
+        :param num_labels: 标签数量
+        :param step: 步数=3
+        :param layer_index: 层序号
+        :param fold: 交叉验证倍数
+        """
+        self.n_estimators = n_estimators
+        self.num_labels = num_labels
+        self.num_forests = num_forests
+        self.layer_index = layer_index
+        self.fold = fold
+        self.step = step
+        self.model = []
+
+    def train(self, train_data, train_label):
+        """
+        :param train_data: 训练数据集
+        :param train_label: 训练数据对应的标签
+        :return:
+        """
+        # 在第一层中，每个森林中有40棵树，然后比上一层增加20棵树，直到树数达到100，最多100棵树
+        n_estimators = min(20 * self.layer_index + self.n_estimators, 100)
+        # 最大深度
+        max_depth = self.step * self.layer_index + self.step
+        # 遍历森林块
+        for forest_index in range(self.num_forests):
+            # 第偶数个森林，用随机森林分类器
+            if forest_index % 2 == 0:
+                clf = RandomForestClassifier(n_estimators=n_estimators,
+                                             criterion="gini",
+                                             max_depth=max_depth,
+                                             n_jobs=-1)
+            # 第奇数个森林，用ExtraTrees分类器
+            else:
+                clf = ExtraTreesClassifier(n_estimators=n_estimators,
+                                           criterion="gini",
+                                           max_depth=max_depth,
+                                           n_jobs=-1)
+            clf.fit(train_data, train_label)
+            self.model.append(clf)
+        self.layer_index += 1
+
+    # 预测
+    def predict(self, test_data):
+        predict_prob = np.zeros([self.num_forests, test_data.shape[0], self.num_labels])
+
+        for forest_index, clf in enumerate(self.model):
+            predict_p = clf.predict_proba(test_data)
+            for j in range(len(predict_p)):
+                predict_prob[forest_index, :, j] = 1 - predict_p[j][:, 0].T
+
+        prob_avg = np.sum(predict_prob, axis=0)
+        prob_avg /= self.num_forests
+        prob_concatenate = predict_prob
+        return [prob_avg, prob_concatenate]
+
+    def train_and_predict(self, train_data, train_label, val_data, test_data):
+        self.train(train_data, train_label)
+        val_avg, val_concatenate = self.predict(val_data)
+        prob_avg, prob_concatenate = self.predict(test_data)
+
+        return [val_avg, val_concatenate, prob_avg, prob_concatenate]
+
---
+++ learner/cascade.py
@@ -0,0 +1,196 @@
+"""
+树级联成森林
+森林最大层数：20
+森林数：2
+"""
+from sklearn.cross_validation import KFold
+# from sklearn.model_selection import KFold
+
+from sklearn.model_selection import train_test_split
+from .measure import *
+from .warpper import KfoldWarpper
+
+
+class Cascade:
+    # 本实验将最大层数（T）设置为20
+    def __init__(self, dataname, max_layer=20, num_forests=2, n_fold=5, step=3):
+        """
+        :param dataname: 数据集名称
+        :param max_layer: 森林最大层数，设为20
+        :param num_forests: 每一层的森林数量，设为2
+        :param n_fold: 每一层交叉验证倍数，设为5
+        :param step: 迭代次数，设为3
+        """
+        self.max_layer = max_layer
+        self.n_fold = n_fold
+        self.step = step
+        self.layer_list = []
+        self.num_forests = num_forests
+        self.dataname = dataname
+        self.eta = []
+        self.model = []
+
+    # 针对六个多标签指标（用supervise表示），计算置信度，公式如论文中的表2所示，结果用alpha表示
+    def compute_confidence(self, supervise, P):
+        """
+        :param supervise: string (e.g. "hamming loss", "one-error")，即指标
+        :param P: array, whose shape is (num_samples, num_labels)
+        :return alpha: array, whose shape is :
+                        (num_samples, ) when supervise is instance-based measure,
+                        and (num_labels, ) when supervise is label-based measure
+        """
+        m, l = P.shape[0], P.shape[1]
+        print("计算置信度，当前层的实例数、标签数：", m, l)
+        if supervise == "hamming loss":
+            alpha = np.sum(np.abs(P - 0.5) + 0.5, axis=0) / m
+        elif supervise == "one-error":
+            alpha = np.max(P, axis=1)
+        elif supervise == "ranking loss" or supervise == "average precision":
+            forward_prod = np.sort(P, axis=1)
+            backward_prod = 1 - forward_prod
+            for j in range(1, l, 1):
+                forward_prod[:, j] = forward_prod[:, j - 1] * P[:, j]
+            for j in range(l - 2, -1, -1):
+                backward_prod[:, j] = backward_prod[:, j + 1] * (1 - P[:, j])
+            alpha = forward_prod[:, l - 1] + backward_prod[:, 0]
+            for j in range(l - 1):
+                alpha += forward_prod[:, j] * backward_prod[:, j + 1]
+        elif supervise == "coverage":
+            backward_prod = 1 - np.sort(P, axis=1)
+            for j in range(l - 2, -1, -1):
+                backward_prod[:, j] = backward_prod[:, j + 1] * (1 - P[:, j])
+            alpha = backward_prod[:, 0]
+            for j in range(l - 1):
+                alpha += j * P[:, j] * backward_prod[:, j + 1]
+            alpha = 1 - alpha / l
+        elif supervise == "macro_auc":
+            forward_prod = np.sort(P, axis=0)
+            backward_prod = 1 - P.copy()
+            for i in range(1, m, 1):
+                forward_prod[i, :] = forward_prod[i - 1, :] * P[i, :]
+            for i in range(m - 2, -1, -1):
+                backward_prod[i, :] = backward_prod[i + 1, :] * (1 - P[i, :])
+            alpha = forward_prod[m - 1, :] + backward_prod[0, :]
+            for i in range(m - 1):
+                alpha += forward_prod[i, :] * backward_prod[i + 1, :]
+        return alpha
+
+    # 在第一层中，每个森林中有40棵树，然后比上一层增加20棵树，直到树数达到100
+    def train(self, train_data_raw, train_label_raw, supervise, n_estimators=40):
+        """
+        :param train_data_raw: array, whose shape is (num_samples, num_features)
+        :param train_label_raw: array, whose shape is (num_samples, num_labels)
+        :param supervise: string, (e.g. "hamming loss", "one-error")
+        :param n_estimators: int, 每个森林块中树的数量，本实验中设为40
+        """
+        train_data = train_data_raw.copy()
+        train_label = train_label_raw.copy()
+        # 标签数取的是训练标签集的列数
+        self.num_labels = train_label.shape[1]
+        best_value = init_supervise(supervise)
+        bad = 0
+
+        best_train_prob = np.empty(train_label.shape)
+        best_concatenate_prob = np.empty([self.num_forests, train_data.shape[0], self.num_labels])
+        print("$" * 50)
+
+        for layer_index in range(self.max_layer):
+            print("训练第" + str(layer_index) + "层ing")
+
+            # K折(n_fold)交叉验证：用sklearn.cross_validation 求kf，此包已经弃用，但有n_folds参数
+            # 将训练/测试数据集划分len(train_label)个互斥子集，
+            #       每次用其中一个子集当作验证集，剩下的len(train_label)-1个作为训练集，
+            #               进行len(train_label)次训练和测试，得到len(train_label)个结果
+            # random_state：随机种子数
+            kf = KFold(len(train_label), n_folds=self.n_fold, random_state=0)
+            # print("cross_validation求得kf:", type(kf), kf)
+
+            # 用from sklearn.model_selection 求kf
+            # shuffle：在每次划分时，是否进行洗牌
+            #     ①若为Falses时，其效果等同于random_state等于整数，每次划分的结果相同
+            #     ②若为True时，每次划分的结果都不一样，表示经过洗牌，随机取样的
+            #
+            # kf = KFold(len(train_label), shuffle=True, random_state=0)
+            # print(kf)
+
+            # 参数：森林数=2，每个森里中的树的数量=40，交叉验证的倍数=5，层序号（1~20，for循环ing），步数=3
+            kfoldwarpper = KfoldWarpper(self.num_forests, n_estimators, self.n_fold, kf, layer_index, self.step)
+
+            prob, prob_concatenate = kfoldwarpper.train(train_data, train_label)
+
+            self.model.append(kfoldwarpper)
+            # 第一层
+            if layer_index == 0:
+                best_train_prob = prob
+                # 指标名称，阈值初值为0.5
+                pre_metric = compute_supervise_vec(supervise, best_train_prob, train_label, 0.5)
+            # 非第一层
+            else:
+                now_metric = compute_supervise_vec(supervise, prob, train_label, 0.5)
+                if supervise == "average precision" or supervise == "macro_auc":
+                    indicator = now_metric < pre_metric
+                else:
+                    indicator = now_metric > pre_metric
+
+                if np.sum(indicator) > 0:
+
+                    confidence = self.compute_confidence(supervise, prob)
+
+                    eta_t = np.mean(confidence[indicator])
+                    train_indicator = confidence < eta_t
+                    if supervise == "hamming loss" or supervise == "macro_auc":
+                        prob[:, train_indicator] = best_train_prob[:, train_indicator]
+                        prob_concatenate[:, :, train_indicator] = best_concatenate_prob[:, :, train_indicator]
+                    else:
+                        prob[train_indicator, :] = best_train_prob[train_indicator, :]
+                        prob_concatenate[:, train_indicator, :] = best_concatenate_prob[:, train_indicator, :]
+                else:
+                    eta_t = 0
+                self.eta.append(eta_t)
+                best_train_prob = prob
+                best_concatenate_prob = prob_concatenate
+                pre_metric = compute_supervise_vec(supervise, best_train_prob, train_label, 0.5)
+            value = compute_supervise(supervise, best_train_prob, train_label, 0.5)
+            back = compare_supervise_value(supervise, best_value, value)
+            if back:
+                bad += 1
+            else:
+                bad = 0
+                best_value = value
+            if bad >= 3:
+                for i in range(bad):
+                    self.model.pop()
+                    self.eta.pop()
+                break
+            # 准备下一层数据
+            prob_concatenate = best_concatenate_prob.transpose((1, 0, 2))
+            prob_concatenate = prob_concatenate.reshape(prob_concatenate.shape[0], -1)
+            train_data = np.concatenate([train_data_raw.copy(), prob_concatenate], axis=1)
+
+    # 针对不同指标，对原始测试数据做预测
+    def predict(self, test_data_raw, supervise):
+        """
+        :param test_data_raw: array, whose shape is (num_test_samples, num_features)
+        :return prob: array, whose shape is (num_test_samples, num_labels)
+        """
+        test_data = test_data_raw.copy()
+        best_prob = np.empty([test_data.shape[0], self.num_labels])
+        best_concatenate_prob = np.empty([self.num_forests, test_data.shape[0], self.num_labels])
+        # zip()函数，两参数中的两迭代对象一一对应并打包成新对象的一个元素
+        for clf, eta_t in zip(self.model, self.eta):
+            prob, prob_concatenate = clf.predict(test_data)
+            confidence = self.compute_confidence(supervise, prob)
+            indicator = confidence < eta_t
+            print(indicator)
+            if supervise == "hamming loss" or supervise == "macro_auc":
+                prob[:, indicator] = best_prob[:, indicator]
+                prob_concatenate[:, :, indicator] = best_concatenate_prob[:, :, indicator]
+            else:
+                prob[indicator, :] = best_prob[indicator, :]
+                prob_concatenate[:, indicator, :] = best_concatenate_prob[:, indicator, :]
+            best_concatenate_prob = prob_concatenate
+            best_prob = prob
+            prob_concatenate = best_concatenate_prob.transpose((1, 0, 2))
+            prob_concatenate = prob_concatenate.reshape(prob_concatenate.shape[0], -1)
+            test_data = np.concatenate([test_data_raw.copy(), prob_concatenate], axis=1)
+        return best_prob
