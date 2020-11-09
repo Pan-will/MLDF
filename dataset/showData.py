c2c2db9cd54e2f137a23d0b624c8623e8855a6d2
pw <15097686925@163.com>
2020-11-09 10:19:07 +0800

    yeast数据集测试完成

--- dataset/showData.py
+++ dataset/showData.py
@@ -15,18 +15,24 @@ def shuffle_index(num_samples):
     test_index = a[length:]
     return [train_index, test_index]
 
+train_data_csv = r'D:\Pycharm2020.1.3\WorkSpace\MLDF\dataset\yeast_data.csv'
+train_label_csv = r'D:\Pycharm2020.1.3\WorkSpace\MLDF\dataset\yeast_label.csv'
 
-data_csv = r'D:\Pycharm2020.1.3\WorkSpace\MLDF\dataset\CAL500_data.csv'
-label_csv = r'D:\Pycharm2020.1.3\WorkSpace\MLDF\dataset\CAL500_label.csv'
-with open(data_csv, encoding='utf-8') as f:
+with open(train_data_csv, encoding='utf-8') as f:
     data = np.loadtxt(f, str, delimiter=",")
-with open(label_csv, encoding='utf-8') as f:
+with open(train_label_csv, encoding='utf-8') as f:
     label = np.loadtxt(f, str, delimiter=",")
 
-print("data矩阵信息：", type(data[0]), data[0].shape, data.shape)
+# 将数据label强制转换为指定的类型，astype函数是在副本上进行，并非修改原数组。
+# 从文件中load出来的数据类型是“class 'numpy.int16'”类型，需要进行类型转化
 label = label.astype("int")
-print("label矩阵信息：", type(label[0]), label[0].shape, label.shape)
+
+
+print("train_data矩阵信息：", type(data[0]), data[0].shape, data.shape)
+print("train_label矩阵信息：", type(label[0]), label[0].shape, label.shape)
+
 num_samples = len(data)
+print("实例数：", len(data))
 train_index, test_index = shuffle_index(num_samples)
 train_data = data[train_index]
 train_label = label[train_index]
@@ -36,6 +42,31 @@ print("train_data", type(train_data), train_data.shape, len(train_data))
 print("train_label", type(train_label), train_label.shape, len(train_label))
 print("test_data", type(test_data), test_data.shape, len(test_data))
 print("test_label", type(test_label), test_label.shape, len(test_label))
+
+
+
+
+
+# data_csv = r'D:\Pycharm2020.1.3\WorkSpace\MLDF\dataset\CAL500_data.csv'
+# label_csv = r'D:\Pycharm2020.1.3\WorkSpace\MLDF\dataset\CAL500_label.csv'
+# with open(data_csv, encoding='utf-8') as f:
+#     data = np.loadtxt(f, str, delimiter=",")
+# with open(label_csv, encoding='utf-8') as f:
+#     label = np.loadtxt(f, str, delimiter=",")
+#
+# print("data矩阵信息：", type(data[0]), data[0].shape, data.shape)
+# label = label.astype("int")
+# print("label矩阵信息：", type(label[0]), label[0].shape, label.shape)
+# num_samples = len(data)
+# train_index, test_index = shuffle_index(num_samples)
+# train_data = data[train_index]
+# train_label = label[train_index]
+# test_data = data[test_index]
+# test_label = label[test_index]
+# print("train_data", type(train_data), train_data.shape, len(train_data))
+# print("train_label", type(train_label), train_label.shape, len(train_label))
+# print("test_data", type(test_data), test_data.shape, len(test_data))
+# print("test_label", type(test_label), test_label.shape, len(test_label))
 
 
 
